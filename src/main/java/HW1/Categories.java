4f38a23fdf25095a7211877a9b69703f5cef3a40
Owner <valprado.v1@gmail.com>
2023-07-03 18:49:07 +0300

    Java OOP testing. 1st home task

---
+++ src/main/java/HW1/Categories.java
@@ -0,0 +1,26 @@
+package HW1;
+
+import java.util.ArrayList;
+
+public class Categories {
+    String categoryName;
+    ArrayList<Goods> setOfGoods;
+
+
+    public Categories(String categoryName) {
+        this.categoryName = categoryName;
+        this.setOfGoods = new ArrayList<>();
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public ArrayList<Goods> getSetOfGoods() {
+        return setOfGoods;
+    }
+
+    void addGoods(Goods goods){
+        setOfGoods.add(goods);
+    }
+}
